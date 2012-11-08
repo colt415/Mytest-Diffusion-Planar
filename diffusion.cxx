808eab28ba391a101505c51a27b2b77e497ae0af
colt415 <yzzmjf@gmail.com>
2012-11-08 13:40:32 -0600

    first commit

---
+++ diffusion.cxx
@@ -0,0 +1,37 @@
+// Diffusion in 2D planar geometry with adiabatic boundary condition.
+//test
+#include <bout.hxx>
+#include <boutmain.hxx>
+
+Field3D T;
+BoutReal k_coff;
+
+int physics_init(bool restarting)
+{
+  Field2D T0;
+  k_coff=1.0;
+
+  mesh->get(T0,"temperature");
+//  mesh->get(g,"g");
+//  Options *options = Options::getRoot();
+//  options = options->getSection("gas");
+//  options->get("k_coff", k_coff, 1.0);
+
+  bout_solve(T,"temperature");
+
+  if(!restarting){
+    T+=T0;
+  }
+  
+  return 0;
+}
+
+
+int physics_run(BoutReal t){
+  
+  mesh->communicate(T);
+
+  ddt(T)=k_coff*Laplacian(T);
+
+  return 0;
+}  
